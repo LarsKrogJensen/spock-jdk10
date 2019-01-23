d61d2af0eb97cdc100cca85ef963abcddd1f6f94
Lars Krog-Jensen <lars.krogjensen@gmail.com>
2019-01-23 16:04:19 +0100

    stuff

--- src/test/groovy/MyMathSpec.groovy
+++ src/test/groovy/MyMathSpec.groovy
@@ -1,5 +1,8 @@
 import spock.lang.Specification
 
+import static org.hamcrest.Matchers.hasKey
+import static spock.util.matcher.HamcrestSupport.that
+
 class MyMathSpec extends Specification {
     def "should add"() {
         given:
@@ -12,4 +15,37 @@ class MyMathSpec extends Specification {
         then:
         result == 12
     }
+
+    def "map should have..."() {
+        given:
+        def map = [1: "one"]
+
+        expect:
+        that map, hasKey(1)
+    }
+
+    def "lambdas"() {
+        given:
+        def list = [1,2,3,4,5]
+
+        def bd = 1.23
+        def sample = new Sample().tap {
+            username = "arne"
+        }
+
+        expect:
+        1 in list
+    }
+}
+
+class Sample {
+
+    String username, email
+
+    List<String> labels = []
+
+    void addLabel(value) {
+        labels << value
+    }
+
 }
